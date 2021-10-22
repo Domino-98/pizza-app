7fcc84bffec411101744ffdd52ca2039189fa882
Dominik Buczek <bdominoo@gmail.com>
2021-10-22 16:17:17 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import Nav from './components/Nav.vue'
+
+export default {
+	name: "App",
+	components: {
+		Nav,
+	},
+	data() {
+		return {
+			msg: "Pizza app",
+		}
+	}
+}
+</script>
+
+<template>
+  	<Nav />
+	<h1>{{ msg }}</h1>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+	name: "Nav",
+}
+</script>
+
+<template>
+
+</template>
