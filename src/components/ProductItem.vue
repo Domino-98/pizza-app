fb3ae12500944b43d6579d07eab9fe492b97ba55
Dominik Buczek <bdominoo@gmail.com>
2021-10-24 17:00:10 +0200

    Style home page, add ProductItem component

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,116 @@
+<template>
+  <div
+    class="
+      relative
+      flex flex-col
+      mx-5
+      w-1/5
+      bg-white
+      pb-5
+      rounded-md
+      justify-between
+    "
+  >
+    <img
+      alt=""
+      class="object-cover h-64 rounded-t-md"
+      :src="'/src/assets/img/' + product.img"
+    />
+    <h2 class="text-2xl py-2">{{ product.name }}</h2>
+    <p class="text-xs">
+      <span v-for="topping in product.toppings" :key="topping"
+        >{{ topping }},
+      </span>
+    </p>
+    <p class="mt-2 text-lg">{{ product.price }} zł</p>
+    <button
+      class="
+        mt-2
+        px-5
+        py-1
+        self-center
+        text-xl
+        bg-transparent
+        border-2 border-orange-500
+        rounded-2xl
+        hover:bg-orange-500 hover:text-white
+        transition
+      "
+      @click="openSelection(product)"
+    >
+      Dodaj do koszyka
+      <fa v-if="product.selectWindow" icon="caret-up" class="text-xl" />
+      <fa v-else icon="caret-down" class="text-xl" />
+    </button>
+    <div
+      v-if="product.selectWindow"
+      class="absolute -bottom-32 bg-white w-full shadow-xl"
+    >
+      <ul class="w-full">
+        <li>
+          <a
+            href="#"
+            class="
+              flex
+              justify-between
+              py-2
+              px-4
+              hover:bg-orange-300
+              transition
+            "
+          >
+            25cm <span>{{ product.price }}zł</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            class="
+              flex
+              justify-between
+              py-2
+              px-4
+              hover:bg-orange-300
+              transition
+            "
+          >
+            32cm <span>{{ product.priceMd }}zł</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            class="
+              flex
+              justify-between
+              py-2
+              px-4
+              hover:bg-orange-300
+              transition
+            "
+          >
+            45cm <span>{{ product.priceLg }}zł</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductList",
+  // eslint-disable-next-line vue/require-prop-types
+  props: ["product"],
+  data() {
+    return {};
+  },
+  methods: {
+    openSelection(product) {
+      product.selectWindow = !product.selectWindow;
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,111 @@
+<template>
+  <div
+    class="
+      bg-header-img bg-no-repeat bg-cover
+      hero
+      bg-opacity-40
+      relative
+      h-screen
+      flex
+      items-center
+      justify-center
+      bg-blend-lighten
+    "
+  >
+    <div
+      class="
+        bg-blend-overlay
+        absolute
+        top-0
+        bottom-0
+        w-full
+        h-full
+        bg-black
+        opacity-50
+      "
+    ></div>
+    <header class="absolute">
+      <h1 class="text-4xl uppercase mb-12 bold tracking-wider text-white">
+        Najlepsza pizza w mieście
+      </h1>
+      <button
+        class="
+          uppercase
+          border-4 border-orange-500
+          text-white text-3xl
+          px-5
+          py-3
+          rounded-xl
+          bg-orange-500
+          hover:bg-orange-400 hover:border-orange-400
+          transition
+        "
+      >
+        Sprawdź menu / Zamów online
+      </button>
+    </header>
+  </div>
+  <h1 class="text-4xl mt-14 uppercase">Najczęściej zamawiane</h1>
+  <div class="flex justify-center items-center mt-10 mb-10 items-stretch">
+    <app-product-item
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+    ></app-product-item>
+  </div>
+</template>
+
+<script>
+import AppProductItem from "../components/ProductItem.vue";
+
+export default {
+  name: "Home",
+  components: {
+    AppProductItem,
+  },
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          img: "capricciosa.jpg",
+          name: "Capricciosa",
+          toppings: [
+            "Sos pomidorowy",
+            "Mozarella",
+            "Szynka",
+            "Pieczarki",
+            "Karczochy",
+          ],
+          price: 28,
+          priceMd: 34,
+          priceLg: 40,
+          selectWindow: false,
+        },
+        {
+          id: 2,
+          img: "margherita.jpg",
+          name: "Margherita",
+          toppings: ["Sos pomidorowy", "Mozarella", "Oliwa"],
+          price: 20,
+          priceMd: 26,
+          priceLg: 32,
+          selectWindow: false,
+        },
+        {
+          id: 3,
+          img: "neapolitan.jpg",
+          name: "Neapolitańska",
+          toppings: ["Sos pomidorowy", "Mozarella", "Pomidory", "Bazylia"],
+          price: 26,
+          priceMd: 32,
+          priceLg: 38,
+          selectWindow: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
